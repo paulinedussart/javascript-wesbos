ceb72c1e3878cc69a0af3c1d3d82a69dbc9904df
Pauline Dussart <paulinedussart04@gmail.com>
2020-10-01 12:42:38 +0200

    learn about .dispatchEvent( new CustomEvent('nameOfTheNewEvent')

--- exercises/57 - Shopping List/shopping.js
+++ exercises/57 - Shopping List/shopping.js
@@ -3,29 +3,43 @@
 const shoppingForm = document.querySelector(".shopping");
 const item = document.getElementById("item");
 const ul = document.querySelector(".list");
-
-
-// we need a array to hold our state 
-// (bunch of data that reflect the current state of my app)
 const items = [];
+
+// we need a array to hold our state (bunch of data that reflect the current state of my app)
 const getItem	= (event) => {
 	event.preventDefault();
-	//console.log(event.currentTarget[0].value);
-	//can grab the value ussing his name attribute
-
+	//can grab the value ussing his name attribute (event.currentTarget[0].value is valid too)
 	const name = event.currentTarget.item.value
+	// if it's empty, stop the function
+	if	(!name) return; 
   const item = {
 		name: name,
 		id: Date.now(),
 		statut: false,
 	}
-	console.log(item);
 	// add the item inside our array of items
 	items.push(item)
-	console.log(items);
 	// clear the form
 	event.currentTarget.item.value = ""
+	// event.target.reset() IS THE SAME AS ABOVE
+	// fire off a custom event (new one) to tell when the items have been updated
+			// first we need to fire they from/attach them to a DOM element 
+	ul.dispatchEvent(new CustomEvent('itemsUpdated'))
+};
+
+const displayItems = (e) => {
+	console.log(e);
+	// iterating over the array to get the names with <li> attributes
+  const html = items.map(item => `<li class="shopping-item">
+	<input value="${item.id}" type="checkbox" checked${item.statut ? checked : null }>
+	<span class="itemName">${item.name}</span>
+	<button><i class="fas fa-trash"></i></button>
+	</li>`).join("")
+	ul.innerHTML = html
 };
 
 
 shoppingForm.addEventListener("submit", getItem)
+ul.addEventListener("itemsUpdated", displayItems)
+
+// LOCAL STORAGE IS A MINI DATABASE IN MY BROWSER
