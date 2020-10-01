a23734e35e0e5eacd54943a5034ead2a1a6e7eec
Pauline Dussart <paulinedussart04@gmail.com>
2020-10-01 10:33:21 +0200

    get the item and clear the form after submit

--- exercises/57 - Shopping List/shopping.js
+++ exercises/57 - Shopping List/shopping.js
@@ -1,11 +1,31 @@
-const addButton = document.querySelector("button");
-const item = document.getElementById("item");
-const ul = document.querySelector("list");
+// Reload whitout refresh page https://parceljs.org/getting_started.html
 
+const shoppingForm = document.querySelector(".shopping");
+const item = document.getElementById("item");
+const ul = document.querySelector(".list");
+
+
+// we need a array to hold our state 
+// (bunch of data that reflect the current state of my app)
+const items = [];
 const getItem	= (event) => {
 	event.preventDefault();
-	console.log(item.value);
+	//console.log(event.currentTarget[0].value);
+	//can grab the value ussing his name attribute
+
+	const name = event.currentTarget.item.value
+  const item = {
+		name: name,
+		id: Date.now(),
+		statut: false,
+	}
+	console.log(item);
+	// add the item inside our array of items
+	items.push(item)
+	console.log(items);
+	// clear the form
+	event.currentTarget.item.value = ""
 };
 
 
-addButton.addEventListener("click", getItem)
+shoppingForm.addEventListener("submit", getItem)
